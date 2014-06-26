e9360d3d2d92e8db2bf1a412a90de1d8a1df2c09
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-06-26 23:16:34 +0200

    fix order

--- app/controllers/backoffice/yos_controller.rb
+++ app/controllers/backoffice/yos_controller.rb
@@ -3,7 +3,7 @@ class Backoffice::YosController < Backoffice::BaseController
   # GET /backoffice/yos
   def index
     @all_yos = Yo.all
-    @yos = Yo.where(:printed_at.exists => true).order('PRINTED_AT DESC')
+    @yos = Yo.where(:printed_at.exists => true).order('printed_at desc')
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @yos }
