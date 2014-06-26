46ba8d0a65da74d49f731a083808514e08bb21d5
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-06-26 10:58:15 +0200

    second commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,15 +1,16 @@
 # SERVERS
 role :web, "ghislain.co"
-role :app, "ghislain.co"            
+role :app, "ghislain.co"
 role :db, "ghislain.co", :primary => true  # This is where Rails migrations will run
 
 # GIT
 set :branch,         'master'
 
-set :rvm_ruby_string, 'ruby-2.0.0-p247'
-set :rvm_type, :user
+set :rvm_ruby_string, 'ruby-2.0.0-p451'
+set :rvm_type, :system
 
-before 'deploy:setup', 'rvm:install_rvm'  # install/update RVM
-before 'deploy:setup', 'rvm:install_ruby' # install Ruby and create gemset (both if missing)
+set :whenever_command, "bundle exec whenever"
+set :whenever_roles, ['app']
+require "whenever/capistrano"
 
 require "rvm/capistrano"
